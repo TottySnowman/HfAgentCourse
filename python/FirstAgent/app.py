406a5751b44f579eb284005150366ee8e2a27650
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2025-02-13 18:05:33 +0100

    added openAI and new internal tool for decision making

--- python/FirstAgent/app.py
+++ python/FirstAgent/app.py
@@ -1,22 +1,37 @@
-from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
+from smolagents import CodeAgent, DuckDuckGoSearchTool, OpenAIServerModel, load_tool, tool
 import datetime
 import requests
 import pytz
 import yaml
+from dotenv import load_dotenv
+import os
 from tools.final_answer import FinalAnswerTool
-
 from Gradio_UI import GradioUI
+import random
 
-# Below is an example of a tool that does nothing. Amaze us with your creativity !
+load_dotenv()
+
+
+# Access variables
 @tool
-def my_cutom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
-    #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+def my_cutom_tool(arg1: str, arg2: int) -> str:  # it's import to specify the return type
+    # Keep this format for the description / args / args description but feel free to modify the tool
+    """A tool that does nothing yet
     Args:
         arg1: the first argument
         arg2: the second argument
     """
     return "What magic will you build ?"
+
+
+@tool
+def flip_a_coin() -> str:
+    """A tool that flips a coin for you. Returns either "Head" or "Tails".
+    Args:
+        None
+    """
+    return random.choice(["Head", "Tails"])
+
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -35,23 +50,23 @@ def get_current_time_in_timezone(timezone: str) -> str:
 
 
 final_answer = FinalAnswerTool()
-model = HfApiModel(
-max_tokens=2096,
-temperature=0.5,
-model_id='https://wxknx1kg971u7k1n.us-east-1.aws.endpoints.huggingface.cloud',# it is possible that this model may be overloaded
-custom_role_conversions=None,
+model = OpenAIServerModel(
+    max_tokens=2096,
+    temperature=0.5,
+    model_id="gpt-4o-mini",
+    api_key=os.getenv("OPENAI_KEY"),
 )
 
-
 # Import tool from Hub
-image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+image_generation_tool = load_tool(
+    "agents-course/text-to-image", trust_remote_code=True)
 
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
-    
+
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[final_answer, get_current_time_in_timezone, flip_a_coin],  # add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
